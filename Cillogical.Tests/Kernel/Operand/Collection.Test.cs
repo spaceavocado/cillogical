cfe395d4ce0209e527b073cb285ffa4fceea212b
David Horak <info@davidhorak.com>
2024-08-07 14:18:18 -0700

    add collection

---
+++ Cillogical.Tests/Kernel/Operand/Collection.Test.cs
@@ -0,0 +1,123 @@
+﻿using Cillogical.Kernel.Operand;
+using Cillogical.Kernel;
+
+namespace Cillogical.UnitTests.Kernel.Operand;
+
+public class CollectionTest
+{
+    public static IEnumerable<object[]> InvalidItemsTestData()
+    {
+        yield return new object[] { new IEvaluable[] { } };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidItemsTestData))]
+    public void InvalidItems(IEvaluable[] items)
+    {
+        Assert.Throws<ArgumentException>(() => new Collection(items));
+    }
+
+    [Theory]
+    [InlineData("==", true)]
+    [InlineData("!=", false)]
+    [InlineData(null, false)]
+    [InlineData(true, false)]
+    public void ShouldBeEscaped(object subject, bool expected)
+    {
+        Assert.Equal(expected, Collection.ShouldBeEscaped(subject, new HashSet<string>() { "==" }));
+    }
+
+    [Theory]
+    [InlineData("==", '\\', "\\==")]
+    [InlineData("==", 'g', "g==")]
+    public void EscapeOperator(string subject, char escapeOperator, string expected)
+    {
+        Assert.Equal(expected, Collection.EscapeOperator(subject, escapeOperator));
+    }
+
+    public static IEnumerable<object[]> EvaluateTestData()
+    {
+        yield return new object[] { new IEvaluable[] { new Value(1) }, new object[] { 1 } };
+        yield return new object[] { new IEvaluable[] { new Value("1") }, new object[] { "1" } };
+        yield return new object[] { new IEvaluable[] { new Value(true) }, new object[] { true } };
+        yield return new object[] { new IEvaluable[] { new Reference("RefA") }, new object[] { "A" } };
+        yield return new object[] { new IEvaluable[] { new Value(1), new Reference("RefA") }, new object[] { 1, "A" } };
+    }
+
+    [Theory]
+    [MemberData(nameof(EvaluateTestData))]
+    public void Evaluate(IEvaluable[] items, object[] expected)
+    {
+        var collection = new Collection(items);
+        Assert.Equal(expected, collection.Evaluate(new Dictionary<string, object> { { "RefA", "A" } }));
+    }
+
+    public static IEnumerable<object[]> SerializeTestData()
+    {
+        yield return new object[] { new IEvaluable[] { new Value(1) }, new object[] { 1 } };
+        yield return new object[] { new IEvaluable[] { new Value("1") }, new object[] { "1" } };
+        yield return new object[] { new IEvaluable[] { new Value(true) }, new object[] { true } };
+        yield return new object[] { new IEvaluable[] { new Reference("RefA") }, new object[] { "$RefA" } };
+        yield return new object[] { new IEvaluable[] { new Value("=="), new Value(1), new Value(1) }, new object[] { "\\==", 1, 1 } };
+        yield return new object[] { new IEvaluable[] { new Value("!="), new Value(1), new Value(1) }, new object[] { "!=", 1, 1 } };
+    }
+
+    [Theory]
+    [MemberData(nameof(SerializeTestData))]
+    public void Serialize(IEvaluable[] items, object[] expected)
+    {
+        var collection = new Collection(items, escapedOperators: new HashSet<string> { "==" });
+        Assert.Equal(expected, collection.Serialize());
+    }
+
+    public static IEnumerable<object[]> SimplifyTestData()
+    {
+        yield return new object[] {
+            new IEvaluable[] { new Reference("RefB") },
+            new Collection(new IEvaluable[] { new Reference("RefB") })
+        };
+        yield return new object[] {
+            new IEvaluable[] { new Reference("RefA") },
+            new object[] { "A" }
+        };
+        yield return new object[] {
+            new IEvaluable[] { new Value(1), new Reference("RefA") },
+            new object[] { 1, "A" }
+        };
+        yield return new object[] {
+            new IEvaluable[] { new Reference("RefA"), new Reference("RefB")  },
+            new Collection(new IEvaluable[] { new Reference("RefA"), new Reference("RefB") })
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(SimplifyTestData))]
+    public void Simplify(IEvaluable[] items, object expected)
+    {
+        var collection = new Collection(items);
+        var simplified = collection.Simplify(new Dictionary<string, object> { { "RefA", "A" } });
+
+        if (simplified is IEvaluable) {
+            Assert.Equal($"{expected}", $"{simplified}");
+        } else {
+            Assert.Equal(expected, simplified);
+        }
+    }
+
+    public static IEnumerable<object[]> StringifyTestData()
+    {
+        yield return new object[] { new IEvaluable[] { new Value(1) }, "[1]" };
+        yield return new object[] { new IEvaluable[] { new Value("1") }, "[\"1\"]" };
+        yield return new object[] { new IEvaluable[] { new Value(true) }, "[true]" };
+        yield return new object[] { new IEvaluable[] { new Reference("RefA") }, "[{RefA}]" };
+        yield return new object[] { new IEvaluable[] { new Value(1), new Reference("RefA") }, "[1, {RefA}]" };
+    }
+
+    [Theory]
+    [MemberData(nameof(StringifyTestData))]
+    public void Stringify(IEvaluable[] items, string expected)
+    {
+        var collection = new Collection(items);
+        Assert.Equal(expected, $"{collection}");
+    }
+}
---
+++ Cillogical/Kernel/Operand/Collection.cs
@@ -0,0 +1,63 @@
+﻿namespace Cillogical.Kernel.Operand;
+using Cillogical.Kernel;
+using System.Linq;
+
+public class Collection : IEvaluable
+{
+    private IEvaluable[] items;
+    private char escapeCharacter;
+    private HashSet<string> escapedOperators;
+
+    public Collection(IEvaluable[] items, char? escapeCharacter = null, HashSet<string>? escapedOperators = null)
+    {
+        if (items.Length < 1)
+        {
+            throw new ArgumentException("collection operand must have at least 1 item");
+        }
+
+        this.items = items;
+        this.escapeCharacter = escapeCharacter ?? '\\';
+        this.escapedOperators = escapedOperators ?? new HashSet<string>();
+    }
+
+    public object Evaluate(Dictionary<string, object>? context = null) =>
+        items.Select((item) => item.Evaluate(context)).ToArray();
+
+    public object Serialize() {
+        var head = items[0].Serialize();
+        if (ShouldBeEscaped(head, escapedOperators)) {
+            head = EscapeOperator((string)head, escapeCharacter);
+        }
+
+        return new object[] { head }.Concat(items.Skip(1).Select((item) => item.Serialize())).ToArray();
+    }
+
+    public object Simplify(Dictionary<string, object>? context = null) {
+        var res = new object?[] { };
+        foreach (var item in items) {
+            var val = item.Simplify(context);
+            if (val is IEvaluable) {
+                return this;
+            }
+
+            Array.Resize(ref res, res.Length + 1);
+            res[res.Length - 1] = val;
+        }
+
+        return res;
+    }
+
+    public override string ToString() =>
+        $"[{String.Join(", ", items.Select((item) => item.ToString()))}]";
+
+    public static bool ShouldBeEscaped(object subject, HashSet<string> escapedOperators)
+    {
+        if (subject == null) {
+            return false;
+        }
+
+        return subject is string && escapedOperators.Contains(subject);
+    }
+
+    public static string EscapeOperator(string op, char escapeOperator) => $"{escapeOperator}{op}";
+}
