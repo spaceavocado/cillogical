89bf7e33ed8d9ec50a7c7f76c2632832fafe6e2b
David Horak <info@davidhorak.com>
2024-08-07 22:43:47 -0700

    add logical, add

---
+++ Cillogical.Tests/Kernel/Expression/Logical/Logical.Test.cs
@@ -0,0 +1,50 @@
+﻿using Cillogical.Kernel.Expression.Logical;
+using Cillogical.Kernel.Operand;
+using Cillogical.Kernel;
+
+namespace Cillogical.UnitTests.Kernel.Expression.Logical;
+
+public class LogicalExpressionMock : LogicalExpression
+{
+    public LogicalExpressionMock(IEvaluable[] operands, string symbol) : base(symbol, symbol, operands) { }
+    public override object Evaluate(Dictionary<string, object>? context)
+    {
+        throw new NotImplementedException();
+    }
+    public override object Simplify(Dictionary<string, object>? context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class LogicalTest
+{
+    public static IEnumerable<object[]> SerializeTestData()
+    {
+        yield return new object[] { "->", new IEvaluable[] { new Value(1), new Value(2) }, new object[] { "->", 1, 2 } };
+        yield return new object[] { "X", new IEvaluable[] { new Value(1) }, new object[] { "X", 1 } };
+    }
+
+    [Theory]
+    [MemberData(nameof(SerializeTestData))]
+    public void Serialize(string op, IEvaluable[] operands, object expected)
+    {
+        var expression = new LogicalExpressionMock(operands, op);
+        Assert.Equal(expected, expression.Serialize());
+    }
+
+    public static IEnumerable<object[]> StringifyTestData()
+    {
+        yield return new object[] { "->", new IEvaluable[] { new Value(1), new Value("2") }, "(1 -> \"2\")" };
+        yield return new object[] { "->", new IEvaluable[] { new Value(1), new Value("2"), new Value(1) }, "(1 -> \"2\" -> 1)" };
+        yield return new object[] { "X", new IEvaluable[] { new Value(1) }, "(X 1)" };
+    }
+
+    [Theory]
+    [MemberData(nameof(StringifyTestData))]
+    public void Stringify(string op, IEvaluable[] operands, object expected)
+    {
+        var expression = new LogicalExpressionMock(operands, op);
+        Assert.Equal(expected, $"{expression}");
+    }
+}
---
+++ Cillogical/Kernel/Expression/Logical/And.cs
@@ -0,0 +1,53 @@
+﻿namespace Cillogical.Kernel.Expression.Logical;
+using Cillogical.Kernel;
+
+public class And : LogicalExpression
+{
+    public And(IEvaluable[] operands, string symbol = "AND") : base("AND", symbol, operands)
+    {
+        if (operands.Length < 2) {
+            throw new ArgumentException("Non unary logical expression must have at least 2 operands");
+        }
+    }
+
+    public override object Evaluate(Dictionary<string, object>? context)
+    {
+        foreach (var operand in operands) {
+            var res = operand.Evaluate(context);
+            if (res is not bool || !(bool)res) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public override object Simplify(Dictionary<string, object>? context)
+    {
+        var simplified = new IEvaluable[] { };
+        foreach (var operand in operands)
+        {
+            var res = operand.Simplify(context);
+            if (res is bool) {
+                if (!(bool)res) {
+                    return false;
+                }
+                continue;
+            }
+
+            Array.Resize(ref simplified, simplified.Length + 1);
+            simplified[simplified.Length - 1] = res is IEvaluable ? (IEvaluable)res : operand;
+        }
+
+        if (simplified.Length == 0) {
+            return true;
+        }
+
+        if (simplified.Length == 1) {
+            return simplified[0];
+        }
+
+        return new And(simplified, symbol);
+    }
+}
