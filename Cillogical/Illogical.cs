58a26abc0658cd88115b4a41a7bf1117b67ce54e
David Horak <info@davidhorak.com>
2024-08-09 10:58:47 -0700

    add illogical base

--- Cillogical/Illogical.cs
+++ Cillogical/Illogical.cs
@@ -1,10 +1,24 @@
-﻿namespace Cillogical;
+﻿using Cillogical.Kernel;
+using Cillogical.Kernel.Operand;
+
+namespace Cillogical;
 
 public class Illogical
 {
-    private Options options;
-
-    public Illogical(Options options) {
-        this.options = options;
+    private Parser parser;
+    public Illogical(
+        Dictionary<Operator, string>? operatorMapping = null,
+        ISerializeOptions? serializeOptions = null,
+        ISimplifyOptions? simplifyOptions = null,
+        char? escapeCharacter = null
+    ) {
+        parser = new Parser(operatorMapping, serializeOptions, simplifyOptions, escapeCharacter);
     }
+
+    public IEvaluable Parse(object[] expression) => parser.Parse(expression);
+    public object? Evaluable(object[] expression, Dictionary<string, object>? context)
+        => parser.Parse(expression).Evaluate(ContextUtils.FlattenContext(context));
+    public object? Simplify(object[] expression, Dictionary<string, object>? context)
+        => parser.Parse(expression).Simplify(ContextUtils.FlattenContext(context));
+    public string Statement(object[] expression) => $"{parser.Parse(expression)}";
 }
